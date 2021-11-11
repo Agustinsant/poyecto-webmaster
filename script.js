650b3a5e3a1545ea8057ab62a2a08da2d088ee54
AgustinSant <agustinmsant@gmail.com>
2021-11-11 11:42:52 -0300

    Transiciones mapa y correcc carrusel responsive

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-$('.gallery-carrousel').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    asNavFor: '.gallery-nav'
-  });
-  $('.gallery-nav').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    asNavFor: '.gallery-carrousel',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
+$(".gallery-carrousel").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".gallery-nav",
+});
+$(".gallery-nav").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: ".gallery-carrousel",
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows:false,
+});
 
+$(".gallery-responsive").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 597,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
   
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
